49471b2e2a1fc0fdbf62cfc08ab8bef102ad004f
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-12-10 13:28:45 +0530

    React-router-dom

---
+++ react31/Backup/Class-5/src/App.js
@@ -0,0 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import Footer from "./components/Footer";
+// import Header  from "./components/Header";
+// import Message  from "./components/Message/Message";
+// import Navbar from './components/Navbar/Navbar';
+import Message from './Message/Message';
+import User from './User/User';
+
+
+function App(){
+  return <div>
+    <h1>App Component</h1>
+    <hr />
+    {/* <Header/>
+    <hr />
+    <Footer/>
+    <hr />
+    <Message/>
+    <hr />
+    <Navbar/> */}
+
+    <Message/>
+    <hr/>
+    <User/>
+    <hr/>
+  </div>
+}
+
+export default App;
--- react31/src/App.js
+++ react31/src/App.js
@@ -4,14 +4,19 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 // import Header  from "./components/Header";
 // import Message  from "./components/Message/Message";
 // import Navbar from './components/Navbar/Navbar';
-import Message from './Message/Message';
-import User from './User/User';
+// import Message from './Message/Message';
+// import User from './User/User';
+import Navbar from './Navbar/Navbar';
+import { Routes , Route , BrowserRouter as Router } from 'react-router-dom';
+import Home from './Header/Home';
+import Contact from './Header/Contact';
+import Login from './Header/Login';
 
 
 function App(){
   return <div>
-    <h1>App Component</h1>
-    <hr />
+    {/* <h1>App Component</h1>
+    <hr /> */}
     {/* <Header/>
     <hr />
     <Footer/>
@@ -20,10 +25,19 @@ function App(){
     <hr />
     <Navbar/> */}
 
-    <Message/>
+    {/* <Message/>
     <hr/>
     <User/>
-    <hr/>
+    <hr/> */}
+<Router>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/home' element={<Home/>}></Route>
+      <Route path='/contact' element={<Contact/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+    </Routes>
+</Router>
   </div>
 }
 
