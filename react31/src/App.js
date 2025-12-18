86ab58e746d88f499eb6de047cbd462fd221d26a
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-12-18 13:14:29 +0530

    Form Handling

--- react31/src/App.js
+++ react31/src/App.js
@@ -1,24 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './navbar/Navbar';
-import ContactApp from './ContactApp/ContactApp';
-import ContactDetails from './ContactApp/ContactDetails';
-import ContactList from './ContactApp/ContactList';
-import Home from './ContactApp/Home';
+// import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+// import Navbar from './navbar/Navbar';
+// import ContactApp from './ContactApp/ContactApp';
+// import ContactDetails from './ContactApp/ContactDetails';
+// import ContactList from './ContactApp/ContactList';
+// import Home from './ContactApp/Home';
+import CreateProduct from './FormHandling/CreateProduct';
 
 
 const App = () => {
   return <div>
-          <Router>
+          {/* <Router>
             <Navbar/>
             <Routes>
               <Route path="/" element={<Home/>}/>
               <Route path="/index" element={<Home/>}/>
               <Route path="/contacts" element={<ContactApp/>}/>
             </Routes>
-          </Router>
+          </Router> */}
+          <h1>App Components</h1>
+          <hr />
+          <CreateProduct/>
         </div>
 }
 
